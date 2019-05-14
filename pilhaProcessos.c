684c3d2e3763fc22055e889700eb3c071dd62355
Daniel dos Anjos <32425523+Daniel-Anjos@users.noreply.github.com>
2019-05-13 23:08:23 -0300

    Update Pilhas

--- pilhaProcessos.c
+++ pilhaProcessos.c
@@ -1,25 +1,41 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<locale.h>
 
 #define tamanho = 5
 
 struct processo{
     int codigo;
-    char nome [20];
+    char nomeProcesso[20];
 };
 
 struct processo fila;
 int opMenu;
 
+void menuPrincipal();
+void adicionaProcesso();
+void removeProcesso();
+void limparPilha();
+void mostrarPilha();
+
 int main (){
+    setlocale (LC_ALL, "Portuguese");
 while (opMenu != 0){
+    mostraPilha();
     menuPrincipal();
     scanf("%d", &opMenu);
 };
 }
+
+void adicionaProcesso(nomeProcesso){
+    printf("Informe o nome do processo a ser enfileirado: \n");
+    scanf("%d", &nomeProcesso);
+}
+
 void menuPrincipal(){
-    printf("\n Informe a Opção desejada: \n");
+    printf("\n *** Informe a Opção desejada: ***\n");
     printf("\n 1- Inserir processo na pilha \n");
     printf("\n 2 - Remover processo da pilha \n");
+    printf("\n 3 - Esvaziar a pilha \n");
     printf("\n 0 - Sair do Programa \n");
 }
