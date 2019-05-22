fde47ab40b9d30ff825433394e2ebfdabd194884
Daniel dos Anjos <daniel.anjos.1989@gmail.com>
2019-05-22 00:07:50 -0300

    Incluído o corpo das funções. Finalizada primeira parte do projeto.

--- pilhaProcessos.c
+++ pilhaProcessos.c
@@ -1,39 +1,89 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<locale.h>
+#include<string.h>
 
-#define tamanho = 5
+#define tamanho 5
 
-struct processo{
-    int codigo;
-    char nomeProcesso[20];
-};
+typedef struct processo{
+    int PID;
+    char nomeProcesso[15];
+}processo;
 
-struct processo fila;
-int opMenu;
-int ini, fim;
+typedef struct tipo_pilha{
+    processo dados[tamanho];
+    int ini, topo;
+}tipo_pilha;
+
+processo elemento;
+tipo_pilha pilha;
 
 void menuPrincipal();
 void adicionaProcesso();
-void removeProcesso();
+processo removeProcesso();
 void limparPilha();
 void mostrarPilha();
 
 int main (){
     setlocale (LC_ALL, "Portuguese");
+int opMenu;
 while (opMenu != 0){
-    mostraPilha();
+    mostrarPilha();
     menuPrincipal();
     scanf("%d", &opMenu);
+    switch (opMenu){
+    case 1:
+        adicionaProcesso(elemento);
+        break;
+    case 2:
+        removeProcesso();
+    case 3:
+        limparPilha();
+    default:
+        printf("Opção Inválida, tente novamente");
+        system("pause");
+        break;
+    }
 };
 }
 
-void adicionaProcesso(nomeProcesso){
-    if (ini == fim){
-        printf ("Desculpe, a pilha está lotada. Remova algum processo ou execute a limpeza.");
+void adicionaProcesso(processo elemento){
+    if (pilha.topo == tamanho){
+        printf ("Desculpe, a pilha está lotada. Remova algum processo ou execute a limpeza. \n");
+        system("pause");
     }else{
-    printf("Informe o nome do processo a ser enfileirado: \n");
-    scanf("%d", &nomeProcesso);
+        printf("Informe o nome do processo a ser enfileirado: \n");
+        scanf("%s", &elemento.nomeProcesso);
+        fflush(stdin);
+        //lembrar de substituir esta declaração por uma função que gere números randômicos
+        elemento.PID += 15;
+        pilha.dados[pilha.topo] = elemento;
+        pilha.topo ++;
+    }
+}
+processo removeProcesso(){
+    if (pilha.ini == pilha.topo){
+        printf ("Desculpe, a pilha está vazia. Insira elementos para remover.\n");
+        system("pause");
+    }else{
+        processo elemento;
+        pilha.topo --;
+        elemento = pilha.dados[pilha.topo];
+        return elemento;
+    }
+}
+void limparPilha(){
+    int i;
+    for(i=tamanho; i > 0; i--){
+        pilha.topo --;
+    }
+    printf("Limpeza de Pilha efetuada com sucesso! \n");
+    system("pause");
+}
+void mostrarPilha(){
+    int i;
+    for(i=0; i < tamanho; i++){
+        printf("%d PID: ", "%s Processo: \n", pilha.dados[i].PID, pilha.dados[i].nomeProcesso);
     }
 }
 
